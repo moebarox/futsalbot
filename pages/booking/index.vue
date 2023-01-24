359075f6fb0f8cc3a6e4d782a92ce3502dd5c120
Hendra Arfiansyah <hendra.arfiansyah@bukalapak.com>
2023-01-24 11:03:07 +0700

    init new project

---
+++ pages/booking/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Hello!
+  </div>
+</template>
+
+<script lang="ts" setup>
+useHead({
+  script: [
+    { src: 'https://telegram.org/js/telegram-web-app.js' },
+  ],
+});
+</script>
---
+++ server/plugins/telegram-bot.ts
@@ -0,0 +1,12 @@
+import { Telegraf } from 'telegraf';
+
+export default defineNitroPlugin(() => {
+  const bot = new Telegraf(<string>process.env.BOT_TOKEN);
+  bot.telegram.getMe();
+  bot.command('hello', (ctx) => ctx.reply('Hello'));
+  bot.launch();
+
+  // Enable graceful stop
+  process.once('SIGINT', () => bot.stop('SIGINT'));
+  process.once('SIGTERM', () => bot.stop('SIGTERM'));
+});
