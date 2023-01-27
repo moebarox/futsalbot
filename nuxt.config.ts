706dbd48d00aeedd343d02a4c70f45927103e181
Hendra Arfiansyah <hendra.arfiansyah@bukalapak.com>
2023-01-27 09:44:24 +0700

    use node server preset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  nitro: {
+    preset: 'node-server'
+  },
   runtimeConfig: {
     botToken: process.env.BOT_TOKEN,
   },
