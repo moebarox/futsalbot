5735eebedfb64a082df782d9458844021f244bf8
Hendra Arfiansyah <hendra.arfiansyah@bukalapak.com>
2023-01-27 11:05:01 +0700

    setup vercel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   nitro: {
-    // preset: 'netlify'
+    // preset: 'vercel'
   },
   runtimeConfig: {
     botToken: process.env.BOT_TOKEN,
