a44946105ba61b2b023111f3454da5c868e6373e
Hendra Arfiansyah <hendra.arfiansyah@bukalapak.com>
2023-01-27 08:31:14 +0700

    add dummy command

--- server/plugins/telegram-bot.ts
+++ server/plugins/telegram-bot.ts
@@ -3,6 +3,7 @@ import { Telegraf } from 'telegraf';
 export default defineNitroPlugin(() => {
   const bot = new Telegraf(<string>process.env.BOT_TOKEN);
   bot.telegram.getMe();
+  bot.command('hello', ctx => ctx.reply('Hallo juga'));
   bot.launch();
 
   // Enable graceful stop
