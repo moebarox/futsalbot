872291635cec1bb1a65bda809c640f5dc74da358
Hendra Arfiansyah <hendra.arfiansyah@bukalapak.com>
2023-01-27 09:59:24 +0700

    use cloudflare pages preset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   nitro: {
-    preset: 'node-server'
+    preset: 'cloudflare_pages'
   },
   runtimeConfig: {
     botToken: process.env.BOT_TOKEN,
