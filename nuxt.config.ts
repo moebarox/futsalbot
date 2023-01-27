2ab57a1d581b5a954d6d77df37ddeace817a9ace
Hendra Arfiansyah <hendra.arfiansyah@bukalapak.com>
2023-01-27 10:37:15 +0700

    setup netlify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   nitro: {
-    preset: 'cloudflare_pages'
+    // preset: 'netlify'
   },
   runtimeConfig: {
     botToken: process.env.BOT_TOKEN,
